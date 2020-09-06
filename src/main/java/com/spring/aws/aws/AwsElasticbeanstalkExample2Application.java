f298155c34d09578d73c98e48eeb377fc5a57e73
Paulo Roberto <paulo.junior@orbia.ag>
2020-09-05 21:58:06 -0300

    first commit

---
+++ src/main/java/com/spring/aws/aws/AwsElasticbeanstalkExample2Application.java
@@ -0,0 +1,13 @@
+package com.spring.aws.aws;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AwsElasticbeanstalkExample2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AwsElasticbeanstalkExample2Application.class, args);
+	}
+
+}
---
+++ src/main/java/com/spring/aws/aws/ServletInitializer.java
@@ -0,0 +1,13 @@
+package com.spring.aws.aws;
+
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
+public class ServletInitializer extends SpringBootServletInitializer {
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(AwsElasticbeanstalkExample2Application.class);
+	}
+
+}
